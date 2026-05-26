bd74d5e6a3ae2115d42054d6cbca9d32cd7103f9
letscodebro <list.append17@gmail.com>
2026-05-26 22:02:01 +0530

    fix: remove Adsterra notification ads

--- src/components/GlobalAds.tsx
+++ src/components/GlobalAds.tsx
@@ -19,10 +19,6 @@ export default function GlobalAds() {
   return (
     <>
       <Script 
-        src="https://pl29559557.effectivecpmnetwork.com/3b/14/55/3b145545d30c91f766d28733d8a73c74.js" 
-        strategy="lazyOnload" 
-      />
-      <Script 
         src="https://quge5.com/88/tag.min.js" 
         data-zone="217476" 
         strategy="lazyOnload" 
